ddf2238cfb4e2477a4064a8da6a59dc6047c13ed
Евгений Салов <s-ee@yandex.ru>
2023-03-02 20:52:16 +0300

    Added Title-model

--- api_yamdb/api/models.py
+++ api_yamdb/api/models.py
@@ -1,3 +1,39 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+    slug = models.SlugField(
+        max_length=50,
+        unique=True
+    )
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=50)
+    slug = models.SlugField(
+        max_length=50,
+        unique=True
+    )
+
+
+class Title(models.Model):
+    name = models.CharField(max_length=120)
+    year = models.IntegerField()
+    description = models.TextField(
+        max_length=250,
+        blank=True
+    )
+    genre = models.ManyToManyField(
+        Genre,
+        on_delete=models.PROTECT,
+        related_name='genre'
+    )
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.PROTECT,
+        related_name='categories'
+    )
+
+    def __str__(self):
+        return self.name
